f1a110678348cc763514eeecfd7ad00f84f62196
SARATH KAUL <kaul.sarath@gmail.com>
2017-08-24 12:46:35 +0530

    Dashboard Features Updated

--- ExpenseManager/angular-src/src/app/app.module.ts
+++ ExpenseManager/angular-src/src/app/app.module.ts
@@ -11,18 +11,19 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import {AuthGuard} from './guards/auth.guard'
+import {AuthGuard} from './guards/auth.guard';
 
-import {ValidateService} from './services/validate.service'
-import {AuthService} from './services/auth.service'
+import {ValidateService} from './services/validate.service';
+import {AuthService} from './services/auth.service';
 import {FlashMessagesModule} from 'angular2-flash-messages';
-import { TransactionsComponent } from './components/dashboard/transactions/transactions.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
 const appRoutes: Routes = [
-  {path:'', component: HomeComponent},
-  {path:'register', component: RegisterComponent},
-  {path:'login', component: LoginComponent},
-  {path:'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
-  {path:'profile', component: ProfileComponent}
+  {path: '', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'transactions', component: TransactionsComponent}
 ]
 
 @NgModule({
@@ -43,7 +44,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule
   ],
-  providers: [ValidateService,AuthService,AuthGuard],
+  providers: [ValidateService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
