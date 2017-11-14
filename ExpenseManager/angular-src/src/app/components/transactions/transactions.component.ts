ed092cd80b36b44a43fbb09f592c9a2d5b6fd02f
rajat188 <rajatsinghal188@gmail.com>
2017-11-14 23:41:16 +0530

    Edit with Account

--- ExpenseManager/angular-src/src/app/components/transactions/transactions.component.ts
+++ ExpenseManager/angular-src/src/app/components/transactions/transactions.component.ts
@@ -3,7 +3,7 @@ import {AuthService} from '../../services/auth.service';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {FlashMessagesService} from 'angular2-flash-messages';
 import {ValidateService} from '../../services/validate.service';
-
+var old_bal=0;
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
@@ -123,8 +123,12 @@ console.log('Hiii');
     });
   }
 
-  update() {
+  update(user) {
+    console.log("hello");
     console.log(this.transactInfo._id);
+
+    const transactions1 = this.transactions;
+    const maps = this.accounts;
     const transacts = {
       first_name : this.first_name,
       last_name : this.last_name,
@@ -134,16 +138,20 @@ console.log('Hiii');
       date : this.date,
       acc_no : this.acc_no
     };
-    console.log(transacts.date);
-    this.authService.update(this._id, transacts).subscribe(res => {
-      console.log(res);
-      this.flashMessage.show('Update  success', {cssClass: 'alert-success', timeout: 3000});
-      window.location.reload();
-    });
-
+      this.upd(user);
   }
+
    editTransaction(id) {
      console.log(id);
+     const transactions1 = this.transactions;
+     const maps = this.accounts;
+     for (let i = 0 ; i < transactions1.length ; i++) {
+       if (transactions1[i]._id === id) {
+         old_bal=transactions1[i].amount;
+
+       }
+     }
+     console.log(old_bal);
      this.authService.editTransact(id).subscribe(res => {
        console.log('data to be edited is' + res.first_name);
        this.flashMessage.show('Edit Success', {cssClass: 'alert-success', timeout: 3000});
@@ -157,6 +165,17 @@ console.log('Hiii');
      });
      document.getElementById('update_button').style.display = 'inline';
      document.getElementById('submit_button').style.display = 'none';
+
+   this.authService.deleteTransact(id).subscribe(data => {
+      if (data.n === 1 ) {
+        for (let i = 0 ; i < transactions1.length ; i++) {
+          if (transactions1[i]._id === id) {
+            transactions1.splice(i, 1);
+            this.flashMessage.show('Delete success', {cssClass: 'alert-success', timeout: 3000});
+          }
+        }
+      }
+    });
    }
 
     addTransaction(user) {
@@ -239,4 +258,81 @@ console.log('Hiii');
        console.log(maps[i].balance);
       }
 }
+upd(user)
+  {
+    const transact = {
+      first_name : user.first_name,
+      last_name : user.last_name,
+      amount: this.amount,
+      nature : this.nature,
+      category : this.category,
+      date : this.date,
+      acc_no : this.acc_no
+    };
+    this.authService.registerTransact(transact).subscribe(data => {
+      if (data.success) {
+        this.flashMessage.show('Transaction Added', {cssClass: 'alert-success', timeout: 3000});
+         // this.refresh();
+        // window.location.reload();
+        this.upd_ac(user);
+      } else {
+        this.flashMessage.show('Something GOING Wrong', {cssClass: 'alert-danger', timeout: 3000});
+      }
+    });
+
+  }
+
+  upd_ac(user)
+  {
+    const maps = this.accounts;
+    console.log(this.acc_no);
+    for (let i = 0; i < maps.length; i++) {
+      console.log(maps[i].acc_no);
+        if (maps[i].acc_no == this.acc_no) {
+          console.log('HIIIIIIIX');
+          const x: number = this.amount;
+          console.log(old_bal+"   "+maps[i].balance+"   "+x);
+          if(x>old_bal)
+          {
+            maps[i].balance = maps[i].balance - (x-old_bal);
+          }
+          else if(x<old_bal)
+          {
+            maps[i].balance = maps[i].balance + (old_bal-x);
+          }
+          const accounts1 = this.accounts;
+          this.authService.deleteAccountss(maps[i]._id).subscribe(data => {
+            if (data.n === 1 ) {
+              for (let i = 0 ; i < accounts1.length ; i++) {
+                if (accounts1[i]._id == maps[i]._id) {
+                  accounts1.splice(i, 1);
+                 this.flashMessage.show('Delete success', {cssClass: 'alert-success', timeout: 3000});
+                }
+              }
+            }
+          });
+          console.log(maps[i].acc_no);
+          const a = {
+            first_name: user.first_name,
+            last_name: user.last_name,
+            acc_no: maps[i].acc_no,
+            acc_type: maps[i].acc_type,
+            balance: maps[i].balance,
+          };
+          this.authService.registerAccount(a).subscribe(data => {
+            if (data.success) {
+              this.flashMessage.show('Account Added', {cssClass: 'alert-success', timeout: 3000});
+              this.refresh();
+              //window.location.reload();
+            } else {
+              this.flashMessage.show('Something GOING Wrong', {cssClass: 'alert-danger', timeout: 3000});
+            }
+          });
+          break;
+        }
+    }
+    for (let i = 0; i < maps.length; i++) {
+     console.log(maps[i].balance);
+    }
+  }
 }
